801ca853a9351d2d13b9ed03fc73f656f2bf5786
Shevchenko <shevchenkoartur745@gmail.com>
2022-06-08 19:34:04 +0300

    update app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,54 @@
 import React, {useEffect} from 'react';
 import {getContractData} from './store/action-creators/near';
 import {useAppDispatch} from './hooks/useAppDispatch';
+import {useAppSelector} from './hooks/useAppSelector';
+import {Box, Button} from '@mui/material';
+import Loading from './components/UI/Loading';
 
 const App = () => {
+    const {contractData, error, isLoading} = useAppSelector(state => state.near);
     const dispatch = useAppDispatch();
+    const centeredStyle = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+    }
 
     useEffect(() => {
         dispatch(getContractData());
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    if (isLoading) {
+        return (
+            <Box sx={centeredStyle}>
+                <Loading />
+            </Box>
+        );
+    }
+
+    if (error) {
+        return (
+            <Box sx={centeredStyle}>
+                {error}
+            </Box>
+        );
+    }
+
     return (
-        <div>
-            App
-        </div>
+        <Box sx={centeredStyle}>
+            {
+                contractData?.currentUser?.accountId
+                    ?
+                    <h2>{contractData.currentUser.accountId}</h2>
+                    :
+                    <>
+                        <p>Please, login to use the app!</p>
+                        <Button variant='contained'>Sign in</Button>
+                    </>
+            }
+        </Box>
     );
 }
 
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -6,7 +6,8 @@ const rootReducer = combineReducers({
 });
 
 export const setupStore = () => configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
