169a8f160617576bf13c6d3e8327fa3311f50ab8
Shevchenko <shevchenkoartur745@gmail.com>
2022-06-14 13:10:49 +0300

    update folder structure

--- src/types/contractWithMethods.ts
+++ src/types/contractWithMethods.ts
@@ -2,6 +2,7 @@ import {Contract} from 'near-api-js';
 import {Post} from './post';
 import {Donation} from './donation';
 
+// Define types for contract with change and view methods
 export interface ContractWithMethods extends Contract {
     findAllPosts: () => Promise<Post[]>;
     newPost: (params: {desc: string, imgUrl: string}) => Promise<Post>;
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,6 +3,7 @@ import {WalletConnection} from 'near-api-js';
 
 export {};
 
+// Define types for window
 declare global {
     interface Window {
         contract: ContractWithMethods;
