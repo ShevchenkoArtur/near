221a03c21381f6e33364a2508c4eb4b87633dbba
Shevchenko <shevchenkoartur745@gmail.com>
2022-06-15 11:02:14 +0300

    refactor signin compnt

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -1,36 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Box, Button, Typography} from '@mui/material';
 import {signIn} from '../../utils';
 import {absoluteCenterStyle} from '../../styles/common';
-import Loading from '../UI/Loading';
 
 const SignIn = () => {
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-
-    const handleSignIn = async () => {
-        try {
-            setIsLoading(true);
-            await signIn();
-            setIsLoading(false);
-        } catch (err) {
-            setIsLoading(false);
-            console.error(err);
-        }
-    }
-
     return (
-        <>
-            {isLoading ?
-                <Loading/>
-                :
-                <Box sx={absoluteCenterStyle}>
-                    <Typography variant='h5'>Please, login to use the app!</Typography>
-                    <Box display='flex'>
-                        <Button onClick={handleSignIn} sx={{margin: '16px auto'}} variant='contained'>Sign in</Button>
-                    </Box>
-                </Box>
-            }
-        </>
+        <Box sx={absoluteCenterStyle}>
+            <Typography variant='h5'>Please, login to use the app!</Typography>
+            <Box display='flex'>
+                <Button onClick={signIn} sx={{margin: '16px auto'}} variant='contained'>Sign in</Button>
+            </Box>
+        </Box>
     );
 };
 
